148b57d203e02995c864d37c6e68ff5c47daf87e
zajacpiotr <33210182+zajacpiotr@users.noreply.github.com>
2018-05-04 01:11:03 +0200

    Add files via upload

---
+++ layout_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $pageTitle ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
