36ee636e5a6c048250a7b342845eae05fcc8bb0a
zajacpiotr <33210182+zajacpiotr@users.noreply.github.com>
2018-05-10 01:30:46 +0200

    Finished email validation

---
+++ Class/Validation.php
@@ -0,0 +1,57 @@
+<?php
+include_once ("DbConfig.php");
+
+class Validation extends DBConfig
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function checkEmpty($data, $fields) 
+    {
+        $msg= null;
+        foreach ($fields as $value) {
+            if (empty($data[$value])) {
+                $msg .= "$value empty <br />";
+            }
+        }
+        return $msg;
+    }
+    public function isValid($field)
+    {
+        if (preg_match("/^[a-zA-ZąęćżźńłóśĄĆĘŁŃÓŚŹŻ\s]+$/", $field)) {
+            if(strlen($field)>2) {
+            return true;
+            }
+        } 
+        return false;
+    }
+    public function read($query)
+    {
+        $stmt = $this->connection->prepare($query);
+        $stmt->execute();
+        foreach ($stmt as $key => $res) {
+            if (count($res)>0) {
+                return true;
+            } else {
+                return false;
+            }
+            
+        }
+        
+    }
+    public function isEmailValid($field)
+    {
+        $pattern = "/^[a-z\'0-9]+([._-][a-z\'0-9]+)*@([a-z0-9]+([._-][a-z0-9]+))+$/";
+        if (preg_match($pattern, $field)) {
+            list($user, $domain) = preg_split("/@/", $field);
+            if (checkdnsrr($domain,"MX")) {
+                return true;
+                } else {
+                return false;
+                }
+        } else {
+            return false;
+        }
+    }
+}
--- register.php
+++ register.php
@@ -1,11 +1,31 @@
 <?php
  $pageTitle = "Register form";
 
- include_once("layout_header.php");
+include_once("layout_header.php");
+include_once("Class/Validation.php");
+
+$email = $msg = $errorMsg = $emailErr = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $validation = new Validation();
+    
+    $email = $_POST['email'];
+    
+    $msg = $validation->checkEmpty($_POST, array('email'));
+    $checkEmail = $validation->isEmailValid($email);
+    
+    if($msg != null) {
+        $errorMsg= $msg; 
+    } elseif (!$checkEmail) {
+            $emailErr= "Podaj prawidłowy adres email";
+        } else {
+            
+        }
+    }
+
 ?>
     <div id="conteiner">
         <form action="register.php" method="post">
-            <div class="form-group">
+            <div class="form-groemailup">
                 <h1>Register </h1>
                 <p>Please fill the form to make new account.</p>
                 <p>
@@ -15,12 +35,12 @@
                 </p>
                 <p>
                     <label for="inputPassword">Password:</label>
-                    <input type="text" name="password" class="form-control" id="inputPassword">
+                    <input type="password" name="password" class="form-control" id="inputPassword">
                     <span class="error"></span>
                 </p>
                 <p>
                     <label for="inputPasswordR">Repeat password:</label>
-                    <input type="text" name="passwordR" class="form-control" id="inputPasswordR">
+                    <input type="password" name="passwordR" class="form-control" id="inputPasswordR">
                     <span class="error"></span>
                 </p>
                 <p>
@@ -36,10 +56,11 @@
                 <p>
                     <label for="inputEmail">Email:</label>
                     <input type="text" name="email" class="form-control" id="inputEmail">
-                    <span class="error"></span>
+                    <span class="error"><?php echo $emailErr; ?></span>
                 </p>
                 <input type="submit" value="Register" class="btn btn-primary">
                 <p class="error">
+                    <?php echo $errorMsg; ?>
                 </p>
             </div>
         </form>
