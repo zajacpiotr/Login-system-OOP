5eb180cba2bfb0fd596aab8924ce36c7d3193a1a
zajacpiotr <33210182+zajacpiotr@users.noreply.github.com>
2018-06-03 02:46:33 +0200

    Added logout button

--- welcome.php
+++ welcome.php
@@ -4,4 +4,16 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
   header("location: index.php");
   exit;
 }
+$pageTitle = "Welcome page";
+include_once("layout_header.php");
 ?>
+    <div class="row page-header">
+        <div class="col-xs-4 col-xs-offset-4">
+            <h2>Hi, <b><?php echo $_SESSION['username']; ?></b>. Welcome to our site.</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-2 col-xs-offset-10">
+            <p><a href="logout.php" class="btn btn-danger">Sign Out</a></p>
+        </div>
+    </div>
